dac8bd38cfc2cc1e4c91269cf85c1cae4c508f39
johannesmenzel <johannes.menzel@posteo.de>
2018-11-18 20:12:14 +0100

    Updated third party library licence

--- SRClasses/SRDynamics.cpp
+++ SRClasses/SRDynamics.cpp
@@ -1,3 +1,32 @@
+/*
+*	copyright 2018, Johannes Menzel, MIT
+*
+*	This is an ongoing modification of the code provided in the Simple Source
+*	library by Bojan Markovic. Licence disclaimer is merged here, since the
+*	classes and functions were merged to one .h and .cpp source file each.
+*	See the original disclaimer below:
+*
+*	copyright 2006, ChunkWare Music Software, OPEN-SOURCE
+*
+*	Permission is hereby granted, free of charge, to any person obtaining a
+*	copy of this software and associated documentation files (the "Software"),
+*	to deal in the Software without restriction, including without limitation
+*	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*	and/or sell copies of the Software, and to permit persons to whom the
+*	Software is furnished to do so, subject to the following conditions:
+*
+*	The above copyright notice and this permission notice shall be included in
+*	all copies or substantial portions of the Software.
+*
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+*	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+*	DEALINGS IN THE SOFTWARE.
+*/
+
 #include "SRDynamics.h"
 namespace SRPlugins {
 
